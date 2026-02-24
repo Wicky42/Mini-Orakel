8bf32e4afb6d488dc51edb074da04b1e050501af
Viktoria Uyanik <viktoria.uyanik@outlook.de>
2026-02-24 14:54:54 +0100

    update Main Class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 void main(){
 
+    // Daten für das Orakel
     String [] dates = { "heute", "morgen", "in einigen Wochen", "in 3 Jahren"};
 
+
+    // Benutzereingaben
     System.out.println("\uD83D\uDD2E Willkommen beim Orakel \uD83D\uDD2E");
 
 
