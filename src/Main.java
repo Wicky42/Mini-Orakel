9fddb3accd50e89a83ba815ddc2e0d21fab3c231
ArbuzovDmtr <arbuzov.dmtr@gmail.com>
2026-02-24 16:17:04 +0100

    test2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ void main(){
     String [] dates = { "heute", "morgen", "in einigen Wochen", "in 3 Jahren"};
 
     System.out.println("\uD83D\uDD2E Willkommen beim Orakel \uD83D\uDD2E");
-
+    System.out.println("smthng");
 
 
 }
