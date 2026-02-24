e562375d28fc17518f73b67f5cae48723289a08e
Viktoria Uyanik <viktoria.uyanik@outlook.de>
2026-02-24 15:37:15 +0100

    add fixed data for oracel

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
 void main(){
 
     // Daten für das Orakel
-    String [] dates = { "heute", "morgen", "in einigen Wochen", "in 3 Jahren"};
+    String [] dates = { "heute", "morgen", "in einigen Wochen", "in 3 Jahren", "in 17 Tagen"};
+    String [] adjectives = { "riesig", "schön" , "wild", "stark", "positiv", "überraschend"};
+    String [] nomsAliveMale = { "Bär", "Löwe", "Mann", "Pinguin", "Zauberer", "Herr"};
+    String [] goodThings = {"Segen", "Liebe", "Gesundheit", "Glück", "Freude"};
 
+    //TODO random int % nutzereingabe instead of 2
+    int index = (int) (Math.random() % 2);
+
+    String output = "%s erscheint ein %s und bringt dir %s";
+    String result = String.format(output, dates[index], nomsAliveMale[index], goodThings[index]);
+    System.out.println(result);
 
     // Benutzereingaben
     System.out.println("\uD83D\uDD2E Willkommen beim Orakel \uD83D\uDD2E");
-
-
 
 }
