874c76ca2f6a4543f2eb57cbbda4f8386059d310
Viktoria Uyanik <viktoria.uyanik@outlook.de>
2026-02-24 15:48:59 +0100

    add possible output

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,27 @@ void main(){
     String [] nomsAliveMale = { "Bär", "Löwe", "Mann", "Pinguin", "Zauberer", "Herr"};
     String [] goodThings = {"Segen", "Liebe", "Gesundheit", "Glück", "Freude"};
 
-    //TODO random int % nutzereingabe instead of 2
-    int index = (int) (Math.random() % 2);
+    int index = 7 % 3;
 
-    String output = "%s erscheint ein %s und bringt dir %s";
-    String result = String.format(output, dates[index], nomsAliveMale[index], goodThings[index]);
-    System.out.println(result);
+    String text;
+    String output = "";
+    switch (index){
+        case 0:
+            text = "%s erscheint ein %s und bringt dir %s . ";
+            output = String.format(text, dates[index], nomsAliveMale[index], goodThings[index]);
+            break;
+        case 1:
+            text = "%s begegnest du einem %s. Das wird %s .";
+            output = String.format(text, dates[index], nomsAliveMale[index], adjectives[index]);
+            break;
+        case 2:
+            text = "%s rettest du einen %s. Das wird %s und gibt dir %s .";
+            output = String.format(text, dates[index], nomsAliveMale[index], adjectives[index], goodThings[index]);
+            break;
+    }
+    System.out.println(output);
+
+
 
     // Benutzereingaben
     System.out.println("\uD83D\uDD2E Willkommen beim Orakel \uD83D\uDD2E");
