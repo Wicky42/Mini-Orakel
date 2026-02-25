3461b5b8958c12f48bf2721f8675a1caab948f49
Viktoria Uyanik <viktoria.uyanik@outlook.de>
2026-02-25 17:02:34 +0100

    fixed index

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 void main(){
 
     // Daten für das Orakel
-    String [] dates = { "heute", "morgen", "in einigen Wochen", "in 3 Jahren", "in 17 Tagen"};
+    String [] dates = { "heute", "morgen", "in einigen Wochen", "in 3 Jahren", "in 17 Tagen", "jetzt"};
     String [] adjectives = { "riesig", "schön" , "wild", "stark", "positiv", "überraschend"};
     String [] nomsAliveMale = { "Bär", "Löwe", "Mann", "Pinguin", "Zauberer", "Herr"};
-    String [] goodThings = {"Segen", "Liebe", "Gesundheit", "Glück", "Freude"};
-
-
-
+    String [] goodThings = {"Segen", "Liebe", "Gesundheit", "Glück", "Freude", "Frieden"};
 
     // Benutzereingaben
     System.out.println("\uD83D\uDD2E Willkommen beim Orakel \uD83D\uDD2E");
@@ -17,39 +14,46 @@ void main(){
     String name = scanner.nextLine();
     System.out.println("Bitte gib dein Lieblingsgetränk ein");
     String getraenk = scanner.nextLine();
-    int number = -1;
+    int number;
     System.out.println("Jetzt gib nur noch eine Zahl zwischen 0 und 4 an.");
     do{
         number = scanner.nextInt();
         if(!isValidNumber(number)){
-            System.out.println("Gib bitte eine Zahl zwischen 0 und 4 an.");
+            System.out.println("Gib bitte eine Zahl größer als 3 an.");
         }
     } while( !isValidNumber(number));
 
 
-    /* Werte die daten aus und ergänze einen Text */
+    /* Werte die daten aus und erstelle einen Text */
 
-    int index = 1234 % number;
+    int index = number % 3;
+    System.out.println(index);
 
     String text = name + ", ";
     String output = "";
     switch (index){
         case 0:
             text += "%s erscheint ein %s und bringt dir %s . ";
-            output = String.format(text, dates[index], nomsAliveMale[index], goodThings[index]);
+            output = String.format(text, dates[getRandom()], nomsAliveMale[getRandom()], goodThings[getRandom()]);
             break;
         case 1:
-            text += "%s begegnest du einem %s. Das wird %s .";
-            output = String.format(text, dates[index], nomsAliveMale[index], adjectives[index]);
+            text += "%s begegnest du einem %s. Das wird %s.";
+            output = String.format(text, dates[getRandom()], nomsAliveMale[getRandom()], adjectives[getRandom()]);
             break;
         case 2:
-            text += "%s rettest du einen %s. Das wird %s und gibt dir %s .";
-            output = String.format(text, dates[index], nomsAliveMale[index], adjectives[index], goodThings[index]);
+            text += "%s rettest du einen %s. Das wird %s und gibt dir %s.";
+            output = String.format(text, dates[getRandom()], nomsAliveMale[getRandom()], adjectives[getRandom()], goodThings[getRandom()]);
             break;
     }
+    output += " Gönn dir eine " + getraenk;
     System.out.println(output);
 }
 
 private boolean isValidNumber(int number){
-    return (number >= 0 && number <= 4);
+    return (number > 3);
+}
+
+private int getRandom(){
+    Random random = new Random();
+    return random.nextInt(6);
 }
